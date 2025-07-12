6eae6d5b1322214f7f0789f2932fb71f0561a44f
gustavo <67645041g@gmail.com>
2025-07-12 17:44:29 -0400

    dale

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-   base: '/'
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    // Para permitir rutas SPA como /task, /task/edit/1
+    historyApiFallback: true,
+  },
 });
