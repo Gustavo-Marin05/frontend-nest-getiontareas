a5152ab974169904b663d3c714e8b697eb8b9ffe
gustavo <67645041g@gmail.com>
2025-07-13 15:18:05 -0400

    vercerl

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "path";
-import { copyFileSync } from "fs";
+
 
 export default defineConfig({
   plugins: [
@@ -10,3 +9,4 @@ export default defineConfig({
     tailwindcss(),
   ]
 });
+
